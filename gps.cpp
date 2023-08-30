1aa6e666548b00350e8c2074e0e88135626518b7
jazz sajan <94254121+jazzsajan@users.noreply.github.com>
2023-08-29 19:53:16 -0500

    Update gps.cpp

--- gps.cpp
+++ gps.cpp
@@ -1,1 +1,173 @@
 //gps code
+
+#include <TinyGPSPlus.h>
+#include <SoftwareSerial.h>
+/*
+   This sample code demonstrates just about every built-in operation of TinyGPSPlus (TinyGPSPlus).
+   It requires the use of SoftwareSerial, and assumes that you have a
+   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
+*/
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 4800;
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+// serial connection to HC12 **update pin numbers??
+SoftwareSerial hc12(10,11); 
+
+// For stats that happen every 5 seconds
+unsigned long last = 0UL; //constant of unsigned long
+
+void setup()
+{
+  Serial.begin(115200);
+  ss.begin(GPSBaud);
+  
+  Serial.println(F("GPS Data"));
+  Serial.println();
+}
+
+//header of hc12 data
+void sendHeaderGPS()
+{ 
+  hc12.print(" LONGITUDE | LATITUDE | ALTITUDE - GPS");
+}
+\
+'
+//send long/lat
+void sendDataGPS(double longitude, double latitude){ 
+  sendHeaderGPS();  
+  hc12.print(longitude + "  " + latitude + "  ");
+  //hc12.println();
+}
+
+//send gps altitude
+void sendDataGPSAltitude(double altitudeGPS){ 
+  hc12.print(altitudeGPS);
+  hc12.println();
+ }
+
+
+void loop()
+{
+  // Dispatch incoming characters
+  while (ss.available() > 0)
+    gps.encode(ss.read());
+
+  if (gps.location.isUpdated()) //LOCATION
+  {
+    Serial.print(F("LOCATION   Fix Age="));
+    Serial.print(gps.location.age());
+    Serial.print(F("ms Raw Lat="));
+    Serial.print(gps.location.rawLat().negative ? "-" : "+");
+    Serial.print(gps.location.rawLat().deg);
+    Serial.print("[+");
+    Serial.print(gps.location.rawLat().billionths);
+    Serial.print(F(" billionths],  Raw Long="));
+    Serial.print(gps.location.rawLng().negative ? "-" : "+");
+    Serial.print(gps.location.rawLng().deg);
+    Serial.print("[+");
+    Serial.print(gps.location.rawLng().billionths);
+    Serial.print(F(" billionths],  Lat="));
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(" Long="));
+    Serial.println(gps.location.lng(), 6);
+    
+
+    sendDataGPS(gps.location.lat(), gps.location.lng()) //SENDING LOCATION DATA
+
+  }
+
+
+  else if (gps.time.isUpdated()) //TIME
+  {
+    Serial.print(F("TIME       Fix Age="));
+    Serial.print(gps.time.age());
+    Serial.print(F("ms Raw="));
+    Serial.print(gps.time.value());
+    Serial.print(F(" Hour="));
+    Serial.print(gps.time.hour());
+    Serial.print(F(" Minute="));
+    Serial.print(gps.time.minute());
+    Serial.print(F(" Second="));
+    Serial.print(gps.time.second());
+    Serial.print(F(" Hundredths="));
+    Serial.println(gps.time.centisecond());
+  }
+
+  else if (gps.speed.isUpdated()) //SPEED
+  {
+    Serial.print(F("SPEED      Fix Age="));
+    Serial.print(gps.speed.age());
+    Serial.print(F("ms Raw="));
+    Serial.print(gps.speed.value());
+    Serial.print(F(" Knots="));
+    Serial.print(gps.speed.knots());
+    Serial.print(F(" MPH="));
+    Serial.print(gps.speed.mph());
+    Serial.print(F(" m/s="));
+    Serial.print(gps.speed.mps());
+    Serial.print(F(" km/h="));
+    Serial.println(gps.speed.kmph());
+  }
+
+
+  else if (gps.altitude.isUpdated()) //ALTITUDE
+  {
+  
+    Serial.print(F(" Altitude Meters="));
+    Serial.print(gps.altitude.meters());
+
+    sendDataGPSAltitude(gps.altitude.meters()); //SENDING ALTITUDE DATA to HC12
+    
+  }
+
+
+  else if (millis() - last > 5000)
+  {
+    Serial.println();
+    if (gps.location.isValid())
+    {
+      static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
+      double distanceToLondon =
+        TinyGPSPlus::distanceBetween(
+          gps.location.lat(),
+          gps.location.lng(),
+          LONDON_LAT, 
+          LONDON_LON);
+      double courseToLondon =
+        TinyGPSPlus::courseTo(
+          gps.location.lat(),
+          gps.location.lng(),
+          LONDON_LAT, 
+          LONDON_LON);
+
+      Serial.print(F("LONDON     Distance="));
+      Serial.print(distanceToLondon/1000, 6);
+      Serial.print(F(" km Course-to="));
+      Serial.print(courseToLondon, 6);
+      Serial.print(F(" degrees ["));
+      Serial.print(TinyGPSPlus::cardinal(courseToLondon));
+      Serial.println(F("]"));
+    }
+
+    Serial.print(F("DIAGS      Chars="));
+    Serial.print(gps.charsProcessed());
+    Serial.print(F(" Sentences-with-Fix="));
+    Serial.print(gps.sentencesWithFix());
+    Serial.print(F(" Failed-checksum="));
+    Serial.print(gps.failedChecksum());
+    Serial.print(F(" Passed-checksum="));
+    Serial.println(gps.passedChecksum());
+
+    if (gps.charsProcessed() < 10)
+      Serial.println(F("WARNING: No GPS data.  Check wiring."));
+
+    last = millis();
+    Serial.println();
+  }
+}
